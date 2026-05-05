e40bc093c22717ba8a2d43c7c34445ae8b2f406a
rishii64 <saptarsismmitra@gmail.com>
2026-05-05 12:44:59 +0530

    refactor: improve MongoDB connection comment formatting

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" })); // important for base64 images
 
-// ==============================
-// MongoDB Connection
-// ==============================
+// ======================== MongoDB Connection ===================
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
